5561367cb1b10a9e346e1ba17aaebc493e93f2b3
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2024-12-16 21:41:56 -0300

    Not entierly sure what changed

--- fish/config.d/02-frontend/20-connection/04-add_path_entries.fish
+++ fish/config.d/02-frontend/20-connection/04-add_path_entries.fish
@@ -1,7 +1,7 @@
-set -e fish_user_paths
-set -U fish_user_paths $HOME/.bin $HOME/.local/bin $HOME/Applications $fish_user_paths
-
-if status --is-login
-    set -gx PATH $PATH ~/.local/bin
-end
-
+# set -e fish_user_paths
+# set -U fish_user_paths $HOME/.bin $HOME/.local/bin $HOME/Applications $fish_user_paths
+#
+# if status --is-login
+#     set -gx PATH $PATH ~/.local/bin
+# end
+#
